031114114ed8ab2d7f72395a1bdc29a291153d5d
coderuzb1 <yashilmashina@gmail.com>
2026-07-15 22:26:09 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("8891233106:AAFpBIUtJnosv3ACH5cFHkuGGjd1i3Pvb8w")
+
+DATABASE_NAME = "saramatch.db"
+
+ADMIN_IDS = [
+    6310532367
+]
+
+MIN_AGE = 18
+MAX_AGE = 50
+
+MAX_PHOTOS = 5
+
+BOT_NAME = "Sara Match ❤️"
