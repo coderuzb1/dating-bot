ff76d66907d41853f4bb2ff213a38cd4802fe833
coderuzb1 <yashilmashina@gmail.com>
2026-08-13 13:26:22 +0500

    Handle messages and callback queries

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,8 +1,20 @@
 from aiogram import Router
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
 
 router = Router()
 
+
 @router.message()
-async def handler(message: Message):
+async def message_handler(message: Message):
     await message.answer("✅ Bot ishlayapti!")
+
+
+@router.callback_query()
+async def callback_handler(callback: CallbackQuery):
+    await callback.answer("✅ Tugma bosildi!", show_alert=False)
+
+    if callback.message:
+        await callback.message.answer(
+            f"🔘 Callback qabul qilindi:\n"
+            f"<code>{callback.data}</code>"
+        )
